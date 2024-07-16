b9baffa252e367ab31541ead181fd15185f250c1
Jackson Severino da Rocha <jackson.severino.rocha@escola.pr.gov.br>
2024-07-16 13:16:56 -0300

    chore: setting docker files

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.10
+
+RUN pip install poetry
+
+WORKDIR /app
+
+COPY pyproject.toml poetry.lock /app/
+
+RUN poetry export -f requirements.txt --output requirements.txt --without-hashes
+
+RUN sed -i 's/ ;.*//' requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY . /app
--- api/main.py
+++ api/main.py
@@ -1,24 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from interfaces import init_routes
 
 app = FastAPI()
 
-
 origins = [
     'http://localhost',
     'http://localhost:3000',
+    'http://localhost:8080',
 ]
-
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['GET', 'POST', 'PUT', 'DELETE'],
-    allow_headers=['Authorization', 'Content-Type'],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
-
 
 init_routes(app=app)
